e8b6c06eb7233fa1f2819a193afe9310cbf8ca1d
Lunex08 <Lunex.08@gmail.com>
2019-02-16 22:28:01 +0300

    Привел к современным стандартам, учел все замечания

--- source/anagram.js
+++ source/anagram.js
@@ -1,38 +1,40 @@
 'use strict';
 
-/* 
- * Группировка слов по анаграммам.
- * Алгоритм: сортировать буквы слова, вставить в словарь пару
- * "отсортированные буквы слова - исходное слово". Сортировать 
- * анаграммы внутри групп и сделать сортировку групп.
-*/
+/**
+ * Находит анаграммы.
+ * @author - Ражев М. А., АПО-22
+ * @param {const} title - массив слов
+ * @returns {String[][]} - массив анаграмм (в каждой группе более одного слова)
+ */
 
-function anagram(arr) {
+const anagram = arr => {
     // Объект для группировки по признаку "слова-анаграммы".
-    var groups = {};
+    let groups = {};
 
     // Рассматриваем все исходные слова
-    for (var i = 0; i < arr.length; i++) {
+    arr.forEach( item => {
       // Разбить слово на буквы, отсортировать и слить обратно
-      var sorted = arr[i].toLowerCase().trim().split('').sort().join(''); // (*)
-      // Удаляем все пробелы. 
+      let sorted = item.toLowerCase().split('').sort().join(''); // (*)
+      // Удаляем все пробелы.
       sorted = sorted.replace(/\s+/g, '');
-      if(groups[sorted] !== undefined) {
+      if(groups[sorted]) {
           // В группе sorted уже есть анаграммы.
-          groups[sorted].push(arr[i]);
-      } else 
-          groups[sorted] =  [ arr[i] ];
-    }
+          groups[sorted].push(item);
+      } else {
+          groups[sorted] =  [item];
+        }
+    });
 
     const result = [];
 
-    for(var property in groups) {
+    for(let property in groups) {
         // По условию задачи, интересуют группы, где более одной анаграммы.
-        if(groups[property].length > 1)
+        if(groups[property].length > 1) {
             result.push(groups[property].slice().sort());
+        }
     }
     // Сортировка групп.
     result.sort();
-      
+
     return result;
 }
